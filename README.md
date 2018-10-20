e3ff8f1db1d925f6d31768284b1358a8b600bd97
Daniele Lasagno <danielelasagno@gmail.com>
2018-10-20 23:29:17 +0200

    Made the base graphics

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Text rpg
+Gioco testuale per browser
+
+## Tema
+Sopravvivenza su un'isola (stile Lost)
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import {greet} from './src/module.mjs';
 
+const inventory = new Map();
+
 console.log('Hello from js!');
 console.log(greet);
